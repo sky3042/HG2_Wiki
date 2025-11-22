830fd32608ad114716c8924754b762f3d5b49d9d
sky3042 <eu_daimonia4738@icloud.com>
2025-11-22 14:35:17 +0900

    Switch to npm

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -1,6 +1,14 @@
 // components/PageAside.tsx
+import { type Block, type ExtendedRecordMap } from 'notion-types'
 
-// 右側のサイドバー（Twitterボタンやシェアボタン）を無効化
-export function PageAside() {
+// 型定義だけ復活させる（中身は使わない）
+type Props = {
+  block: Block
+  recordMap: ExtendedRecordMap
+  isBlogPost: boolean
+}
+
+export function PageAside(props: Props) {
+  // 何も表示しない
   return null
 }
